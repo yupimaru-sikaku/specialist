9f4624aadba64eb9f4b1a12b048532d60376b89c
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-09-09 19:17:58 +0900

    🎉プロジェクト立ち上げ

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily: 'Verdana, sans-serif',
+      }}
+    >
+      <NotificationsProvider limit={3}>
+        <Component {...pageProps} />
+      </NotificationsProvider>
+    </MantineProvider>
+  );
+}
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    'src/pages/**/*.{js,ts,jsx,tsx}',
+    'src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      xs: '576px',
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+      xl: '1400px',
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwindcss-fluid-spacing'),
+    require('@tailwindcss/line-clamp'),
+  ],
+  corePlugins: {
+  // MantineUIとTailwindCSSを一緒に使うとコンポーネントがうまく表示されない。その対策
+    preflight: false,
+  },
+  darkMode: 'class',
+};
