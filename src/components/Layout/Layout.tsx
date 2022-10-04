f65e59d67dfd1c28306909638a0301251edb4cd5
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-10-04 18:31:07 +0900

    修正

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import { Navbar } from 'src/components/Layout/Navbar';
 import { Footer } from 'src/components/Layout/Footer';
-import { DownUpScreenTransition } from 'src/components/Common/DownUpScreenTransition';
 
 type Props = {
   title: string;
@@ -19,9 +18,7 @@ export const Layout: NextPage<Props> = ({
       <Head>
         <title>{title}</title>
       </Head>
-      <DownUpScreenTransition>
-        <Navbar />
-      </DownUpScreenTransition>
+      <Navbar />
       <main className="my-12 flex-grow px-5 sm:px-10 md:my-20 md:px-20">
         {children}
       </main>
