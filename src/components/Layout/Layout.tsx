36c8302511c1a356424a7c9321fb19b66f419ce7
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-10-26 15:41:33 +0900

    モーダルのアニメ追加

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,8 +3,6 @@ import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import { Navbar } from 'src/components/Layout/Navbar';
 import { Footer } from 'src/components/Layout/Footer';
-import { MainMenuCarousel } from 'src/components/Main/MainMenuCarousel';
-import { MainImageCarousel } from 'src/components//Main/MainImageCarousel';
 
 type Props = {
   title: string;
@@ -16,7 +14,7 @@ export const Layout: NextPage<Props> = ({
   children,
 }) => {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex flex-col">
       <Head>
         <title>{title}</title>
       </Head>
--- src/components/Layout/NavbarModal.tsx
+++ src/components/Layout/NavbarModal.tsx
@@ -3,123 +3,110 @@ import { headerLink } from 'src/utils/headerLink';
 import { useRouter } from 'next/router';
 import { IconPlus, IconX } from '@tabler/icons';
 import { Accordion, ActionIcon } from '@mantine/core';
-import { newsList } from 'src/utils/mainMenuCarouselList';
+import { newsList, recordList } from 'src/utils/mainMenuCarouselList';
 import Link from 'next/link';
 import { BaseText } from '../Common/BaseText';
 
-type Props = {
-  setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
-export const NavBarModal = ({ setIsModal }: Props) => {
-  const router = useRouter();
-  useEffect(() => {
-    () => setIsModal(false);
-  }, []);
+export const NavBarModal = () => {
   return (
-    <div className="h-modal z-100 fixed top-0 right-0 left-0 flex w-full items-center justify-center overflow-scroll overflow-y-auto overflow-x-hidden md:inset-0 md:h-full">
-      <div className="relative h-full w-full max-w-md p-4 md:h-auto overflow-scroll">
-        <div className="relative rounded-l shadow">
-          <ActionIcon
-            onClick={() => setIsModal(false)}
-            className="absolute top-3 right-2.5 p-1.5 hover:bg-gray-200 hover:text-gray-900"
-            size="lg"
-          >
-            <IconX color="white" />
-          </ActionIcon>
-          <div className="rounded-t py-4 px-6">
-            <h3 className="text-base font-semibold text-gray-100 lg:text-xl">
-              メニュー
-            </h3>
-          </div>
-          <div className="p-6">
-            <Accordion
-              chevron={<IconPlus size={16} />}
-              styles={{
-                chevron: {
-                  color: 'white',
-                  '&[data-rotate]': {
-                    transform: 'rotate(45deg)',
-                  },
-                },
-                item: {
-                  border: 'none',
-                },
-              }}
-            >
-              <Accordion.Item value="RECORD">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    RECORD
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    実績
-                  </BaseText>
-                </Accordion.Control>
-              </Accordion.Item>
-              <Accordion.Item value="NEWS">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    NEWS
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    ニュース
-                  </BaseText>
-                </Accordion.Control>
-                {newsList.map((content) => (
-                  <Accordion.Panel key={content.label}>
-                    <Link href={content.link}>
-                      <a className="text-center">
-                        <BaseText content="middle">{content.label}</BaseText>
-                      </a>
-                    </Link>
-                  </Accordion.Panel>
-                ))}
-              </Accordion.Item>
-              <Accordion.Item value="CHARITY">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    CHARITY
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    チャリティー
-                  </BaseText>
-                </Accordion.Control>
-              </Accordion.Item>
-              <Accordion.Item value="SPONSOR">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    SPONSOR
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    スポンサー
-                  </BaseText>
-                </Accordion.Control>
-              </Accordion.Item>
-              <Accordion.Item value="SUPPORT">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    SUPPORT
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    サポート
-                  </BaseText>
-                </Accordion.Control>
-              </Accordion.Item>
-              <Accordion.Item value="MEMBER">
-                <Accordion.Control>
-                  <BaseText content="middle" color="white">
-                    MEMBER
-                  </BaseText>
-                  <BaseText content="small" color="white">
-                    メンバー
-                  </BaseText>
-                </Accordion.Control>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Accordion
+        chevron={<IconPlus size={16} />}
+        styles={{
+          chevron: {
+            color: 'white',
+            '&[data-rotate]': {
+              transform: 'rotate(45deg)',
+            },
+          },
+          item: {
+            border: 'none',
+          },
+        }}
+      >
+        <Accordion.Item value="RECORD">
+          <Accordion.Control>
+            <BaseText content="middle" color="white">
+              RECORD
+            </BaseText>
+            <BaseText content="small" color="white">
+              実績
+            </BaseText>
+          </Accordion.Control>
+          {recordList.map((content) => (
+            <Accordion.Panel key={content.label}>
+              <Link href={content.link}>
+                <a className="text-center">
+                  <BaseText content="middle">{content.label}</BaseText>
+                </a>
+              </Link>
+            </Accordion.Panel>
+          ))}
+        </Accordion.Item>
+        <Accordion.Item value="NEWS">
+          <Accordion.Control>
+            <BaseText content="middle" color="white">
+              NEWS
+            </BaseText>
+            <BaseText content="small" color="white">
+              ニュース
+            </BaseText>
+          </Accordion.Control>
+          {newsList.map((content) => (
+            <Accordion.Panel key={content.label}>
+              <Link href={content.link}>
+                <a className="text-center">
+                  <BaseText content="middle">{content.label}</BaseText>
+                </a>
+              </Link>
+            </Accordion.Panel>
+          ))}
+        </Accordion.Item>
+        <Link href="/charity">
+          <a>
+            <Accordion.Item value="CHARITY">
+              <Accordion.Control>
+                <BaseText content="middle" color="white">
+                  CHARITY
+                </BaseText>
+                <BaseText content="small" color="white">
+                  チャリティー
+                </BaseText>
+              </Accordion.Control>
+            </Accordion.Item>
+          </a>
+        </Link>
+        <Accordion.Item value="SPONSOR">
+          <Accordion.Control>
+            <BaseText content="middle" color="white">
+              SPONSOR
+            </BaseText>
+            <BaseText content="small" color="white">
+              スポンサー
+            </BaseText>
+          </Accordion.Control>
+        </Accordion.Item>
+        <Accordion.Item value="SUPPORT">
+          <Accordion.Control>
+            <BaseText content="middle" color="white">
+              SUPPORT
+            </BaseText>
+            <BaseText content="small" color="white">
+              サポート
+            </BaseText>
+          </Accordion.Control>
+        </Accordion.Item>
+        <Accordion.Item value="MEMBER">
+          <Accordion.Control>
+            <BaseText content="middle" color="white">
+              MEMBER
+            </BaseText>
+            <BaseText content="small" color="white">
+              メンバー
+            </BaseText>
+          </Accordion.Control>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 };
