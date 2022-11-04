a62930e5efa59eb9adfeaea60400739264326779
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-11-04 12:16:43 +0900

    修正

--- src/components/Main/MainSponsorList.tsx
+++ src/components/Main/MainSponsorList.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { BaseText } from 'src/components/Common/BaseText';
+import { sponsorLink } from 'src/utils/sponsorLink';
 
 export const MainSponsorList = () => {
   return (
@@ -12,42 +13,22 @@ export const MainSponsorList = () => {
         </BaseText>
       </h2>
       <div className="p-vw-20" />
-      <ul className="grid gap-8 items-center" style={{ gridTemplateColumns: '1fr 1fr' }}>
-        <li>
-          <Link href="/">
-            <a>
-              <Image src="/ishitomi.png" width={200} height={150} />
+      <ul className="grid gap-8" style={{ gridTemplateColumns: '1fr 1fr' }}>
+        {sponsorLink.map((content) => (
+          <Link href={content.link} key={content.name}>
+            <a className="flex flex-col gap-5">
+              <Image
+                src={content.src}
+                width={50}
+                height={50}
+                layout="responsive"
+              />
+              <BaseText content="small" align="center" color="dark">
+                {content.name}
+              </BaseText>
             </a>
           </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>
-              <Image src="/novarex.jpg" width={200} height={100} />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>
-              <Image src="/maf.png" width={150} height={150} />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>
-              <Image src="/carriarx.png" width={150} height={100} />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>
-              <Image src="/umehanakai.png" width={150} height={150} />
-            </a>
-          </Link>
-        </li>
+        ))}
       </ul>
       <div className="p-vw-20" />
     </div>
