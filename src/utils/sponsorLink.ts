b35ee9a42abcb4d017453a62da176bdd92a65ca7
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-11-04 12:18:44 +0900

    修正

--- src/utils/sponsorLink.ts
+++ src/utils/sponsorLink.ts
@@ -1,7 +1,7 @@
 export const sponsorLink = [
-  { name: '石富プロパティー', link: '#', src: '/ishitomi.png' },
-  { name: '合同会社MAF', link: '#', src: '/maf.png' },
-  { name: '株式会社JVU', link: '#', src: '/jvu.png' },
-  { name: '医療法人社団梅華会', link: '#', src: '/umehanakai.png' },
-  { name: '合同会社キャリアークス', link: '#', src: '/carriarx.png' },
+  { name: '石富プロパティー', link: '#', src: '/ishitomi.webp' },
+  { name: '合同会社MAF', link: '#', src: '/maf.webp' },
+  { name: '株式会社JVU', link: '#', src: '/jvu.webp' },
+  { name: '医療法人社団梅華会', link: '#', src: '/umehanakai.webp' },
+  { name: '合同会社キャリアークス', link: '#', src: '/carriarx.webp' },
 ];
