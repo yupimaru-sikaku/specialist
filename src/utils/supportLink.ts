b9f76ce6fd566f918ca05ec1bf9bf092dd74173d
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-11-04 09:35:22 +0900

    画像最適化

--- src/utils/supportLink.ts
+++ src/utils/supportLink.ts
@@ -1,7 +1,14 @@
 export const supportLink = [
-  { name: '石富プロパティー', link: '#', src: '/ishitomi.png' },
-  { name: '合同会社MAF', link: '#', src: '/maf.png' },
-  { name: '株式会社JVU', link: '#', src: '/jvu.png' },
-  { name: '医療法人社団梅華会', link: '#', src: '/umehanakai.png' },
-  { name: '合同会社キャリアークス', link: '#', src: '/carriarx.png' },
+  { name: '石富プロパティー', link: '#', src: '/ishitomi.webp' },
+  { name: '合同会社MAF', link: '#', src: '/maf.webp' },
+  { name: '株式会社JVU', link: '#', src: '/jvu.webp' },
+  { name: '医療法人社団梅華会', link: '#', src: '/umehanakai.webp' },
+  { name: '合同会社キャリアークス', link: '#', src: '/carriarx.webp' },
+  {
+    name: '株式会社CHAINONエンターテインメント',
+    link: '#',
+    src: '/CHAINON.webp',
+  },
+  { name: '一般社団法人日本美容改革推進協会', link: '#', src: '/jpa.webp' },
+  { name: 'マイケル・ジャクトン', link: '#', src: '/michael_jackton.webp' },
 ];
