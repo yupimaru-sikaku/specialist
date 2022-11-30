f03fc54c966fe21782d4cde927875c60a892a69d
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-11-30 17:25:12 +0900

    push

---
+++ src/components/Common/index.ts
@@ -0,0 +1,5 @@
+export { FormTextInput } from 'src/components/Common/FormTextInput';
+export { FormTextArea } from 'src/components/Common/FormTextArea';
+export { FormPasswordInput } from 'src/components/Common/FormPasswordInput';
+export { BaseButton } from 'src/components/Common/BaseButton';
+export { BaseText } from 'src/components/Common/BaseText';
